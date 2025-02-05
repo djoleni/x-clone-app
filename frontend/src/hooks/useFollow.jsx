ba809aad82acad645590180e921258ff48d352c7
Djoleni <stevanojlo@gmail.com>
2025-02-05 21:03:39 +0100

    useFollow hook added

---
+++ frontend/src/hooks/useFollow.jsx
@@ -0,0 +1,42 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+const useFollow = () => {
+
+    const queryClient = useQueryClient();
+
+    const {mutate:follow, isPending} = useMutation({
+        mutationFn: async (userId) => {
+            try {
+               const res = await fetch(`/api/users/follow/${userId}`,
+                {
+                    method: "POST",    
+                })
+
+                const data = await res.json();
+                if(!res.ok) {throw new Error(data.error || "Something went wrong")}
+               
+                return data;
+               
+            } catch (error) {
+                throw new Error(error)
+            }
+        },
+        onSuccess: () => {
+          Promise.all([
+            queryClient.invalidateQueries({queryKey: ['suggestedUsers']}), //kada nekog zapratimo ne zelimo da ga vidimo vise u suggested
+            queryClient.invalidateQueries({queryKey: ['authUser']}) //kada nekog zapratimo recimo sa profila, zelimo da follow postane unfollow a zato nam treba refetch authUser data
+          ])
+         
+        },
+        onError: () => {
+            toast.error(error.message);
+        }
+    })
+
+    return {follow, isPending};
+
+
+};
+
+export default useFollow
